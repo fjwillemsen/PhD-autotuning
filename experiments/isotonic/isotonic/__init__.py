3b363bf9a2d48acc32f03f3604f93069e001c0ee
fjwillemsen <fjwillemsen@icloud.com>
2022-07-07 07:24:10 +0200

    Moved experiments to own repository

--- experiments/isotonic/isotonic/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lp_isotonic_regression import LpIsotonicRegression
-from .binomial_isotonic_regression import BinomialIsotonicRegression
-
-
-__all__ = ['LpIsotonicRegression', 'BinomialIsotonicRegression']
